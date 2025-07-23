dae3d728ea4ab267c5a8d9e67edff66555e6e0a5
eleven-dimension <3096152319@qq.com>
2025-07-23 22:20:43 +0800

    chore: expose existing classes in `layers` via `__init__.py`

--- src/layers/__init__.py
+++ src/layers/__init__.py
@@ -1,3 +1,6 @@
+from .closure_mlp import ClosureMLP
+from .gated_node_aggregator import GatedNodeAggregator
+from .inv_edge_encoder import InvEdgeEncoder
 from .monotonic_function import MonotonicFunction
 
-__all__ = ["MonotonicFunction"]
+__all__ = ["ClosureMLP", "GatedNodeAggregator", "InvEdgeEncoder", "MonotonicFunction"]
